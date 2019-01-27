e1a08e20b8148c7ba96d4371f23299208c64b0ff
b0z1b <kevinhryang@alumni.ubc.ca>
2019-01-26 22:56:22 -0800

    refactored Main to use ttmini class, running into runtime bugs

--- Main.py
+++ Main.py
@@ -14,8 +14,8 @@ class ttmini:
         self.mBoard = [[None,None,None],[None,None,None],[None,None,None]]
         for i in range (0,3):
             for j in range (0,3):
-                self.mBoard[i][j] = Button(tk, text=" ", font=('Times 20 bold'), bg='gray', fg='white', height=4, width=8,command=lambda: ttt(self.mBoard[i][j]))
-                self.mBoard[i][j].grid(row=i, column=j, sticky=S + N + E + W)
+                self.mBoard[i][j] = Button(tk, text=" ", font=('Times 20 bold'), bg='gray', fg='white', height=4, width=8,command=lambda: ttmini.ttt(self,self.mBoard[i][j]))
+                self.mBoard[i][j].grid(row=i+1, column=j, sticky=S + N + E + W)
 
     def getWinner(self):
         return self.winner
@@ -38,50 +38,59 @@ class ttmini:
     def isTaken(self,xPos,yPos):
         return self.mBoard[xPos][yPos]["text"] != ""
 
-    def click(self,player,xPos,yPos):
+    # def click(self,player,xPos,yPos):
+    #     global bclick
+    #     if ~(ttmini.isTaken(xPos,yPos)):
+    #         if player:
+    #             self.mBoard[xPos][yPos]["text"]= "X"
+    #         else:
+    #             self.mBoard[xPos][yPos]["text"] = "O"
+    #         bclick = ~bclick
+
+    def ttt(self, buttons):
         global bclick
-        if ~(ttmini.isTaken(xPos,yPos)):
-            if player:
-                self.mBoard[xPos][yPos]["text"]= "X"
-            else:
-                self.mBoard[xPos][yPos]["text"] = "O"
-            bclick = ~bclick
-#
-# def ttt(buttons):
-#     global bclick
-#     if buttons["text"] == " " and bclick == True:
-#         buttons["text"] = "X"
-#         bclick = False
-#     if buttons["text"] == " " and bclick == False:
-#         buttons["text"] = "O"
-#         bclick = True
-#
-#     if (button1['text'] == 'X' and button2['text'] == 'X' and button3['text'] == 'X' or
-#             button4['text'] == 'X' and button5['text'] == 'X' and button6['text'] == 'X' or
-#             button7['text'] == 'X' and button8['text'] == 'X' and button9['text'] == 'X' or
-#             button1['text'] == 'X' and button5['text'] == 'X' and button9['text'] == 'X' or
-#             button3['text'] == 'X' and button5['text'] == 'X' and button7['text'] == 'X' or
-#             button1['text'] == 'X' and button2['text'] == 'X' and button3['text'] == 'X' or
-#             button1['text'] == 'X' and button4['text'] == 'X' and button7['text'] == 'X' or
-#             button2['text'] == 'X' and button5['text'] == 'X' and button8['text'] == 'X' or
-#             button7['text'] == 'X' and button6['text'] == 'X' and button9['text'] == 'X'):
-#         tkinter.messagebox.showinfo("Player X", 'Winner is X !!!!')
-#         sys.exit()
-#
-#     if (button1['text'] == 'O' and button2['text'] == 'O' and button3['text'] == 'O' or
-#             button4['text'] == 'O' and button5['text'] == 'O' and button6['text'] == 'O' or
-#             button7['text'] == 'O' and button8['text'] == 'O' and button9['text'] == 'O' or
-#             button1['text'] == 'O' and button5['text'] == 'O' and button9['text'] == 'O' or
-#             button3['text'] == 'O' and button5['text'] == 'O' and button7['text'] == 'O' or
-#             button1['text'] == 'O' and button2['text'] == 'O' and button3['text'] == 'O' or
-#             button1['text'] == 'O' and button4['text'] == 'O' and button7['text'] == 'O' or
-#             button2['text'] == 'O' and button5['text'] == 'O' and button8['text'] == 'O' or
-#             button7['text'] == 'O' and button6['text'] == 'O' and button9['text'] == 'O'):
-#         tkinter.messagebox.showinfo("Player O", 'Winner is O !!!!')
-#         sys.exit()
-#
-#
+        if buttons["text"] == " " and bclick == True:
+            buttons["text"] = "X"
+            bclick = False
+        if buttons["text"] == " " and bclick == False:
+            buttons["text"] = "O"
+            bclick = True
+
+        hasWon = self.checkWinCond()
+        if hasWon == "X" :
+            tkinter.messagebox.showinfo("Player X", 'Winner is X !!!!')
+            sys.exit()
+        if hasWon == "O":
+            tkinter.messagebox.showinfo("Player O", 'Winner is O !!!!')
+            sys.exit()
+
+    # # if (button1['text'] == 'X' and button2['text'] == 'X' and button3['text'] == 'X' or
+    # #         button4['text'] == 'X' and button5['text'] == 'X' and button6['text'] == 'X' or
+    # #         button7['text'] == 'X' and button8['text'] == 'X' and button9['text'] == 'X' or
+    # #         button1['text'] == 'X' and button5['text'] == 'X' and button9['text'] == 'X' or
+    # #         button3['text'] == 'X' and button5['text'] == 'X' and button7['text'] == 'X' or
+    # #         button1['text'] == 'X' and button2['text'] == 'X' and button3['text'] == 'X' or
+    # #         button1['text'] == 'X' and button4['text'] == 'X' and button7['text'] == 'X' or
+    # #         button2['text'] == 'X' and button5['text'] == 'X' and button8['text'] == 'X' or
+    # #         button7['text'] == 'X' and button6['text'] == 'X' and button9['text'] == 'X'):
+    #      tkinter.messagebox.showinfo("Player X", 'Winner is X !!!!')
+    #     sys.exit()
+    #
+    # if (button1['text'] == 'O' and button2['text'] == 'O' and button3['text'] == 'O' or
+    #         button4['text'] == 'O' and button5['text'] == 'O' and button6['text'] == 'O' or
+    #         button7['text'] == 'O' and button8['text'] == 'O' and button9['text'] == 'O' or
+    #         button1['text'] == 'O' and button5['text'] == 'O' and button9['text'] == 'O' or
+    #         button3['text'] == 'O' and button5['text'] == 'O' and button7['text'] == 'O' or
+    #         button1['text'] == 'O' and button2['text'] == 'O' and button3['text'] == 'O' or
+    #         button1['text'] == 'O' and button4['text'] == 'O' and button7['text'] == 'O' or
+    #         button2['text'] == 'O' and button5['text'] == 'O' and button8['text'] == 'O' or
+    #         button7['text'] == 'O' and button6['text'] == 'O' and button9['text'] == 'O'):
+    #     tkinter.messagebox.showinfo("Player O", 'Winner is O !!!!')
+    #     sys.exit()
+
+
 buttons = StringVar()
+ttmini()
 #
 # button1 = Button(tk, text=" ", font=('Times 20 bold'), bg='gray', fg='white', height=4, width=8,
 #                  command=lambda: ttt(button1))
