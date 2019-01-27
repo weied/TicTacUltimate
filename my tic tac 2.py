b1e068746e2e22028bb446c56194cda8817b0649
Edward Wei <34778238+weied@users.noreply.github.com>
2019-01-26 20:22:24 -0800

    Update my tic tac 2.py
    Buttons Implemented!

--- my tic tac 2.py
+++ my tic tac 2.py
@@ -33,8 +33,10 @@ def ttt(buttons):
           button1['text'] == 'X' and button4['text'] == 'X' and button7['text'] == 'X'or
           button2['text'] == 'X' and button5['text'] == 'X' and button8['text'] == 'X'or
           button7['text'] == 'X' and button6['text'] == 'X' and button9['text'] == 'X'):
-          tkinter.messagebox.showinfo("Player X",'Winner is X !!!!')
-          restart_program()
+          if tkinter.messagebox.askyesno("Player X has won!",'Restart?') == True:
+               restart_program()
+          else:
+               sys.exit()
           
      if(button1['text'] == 'O' and button2['text'] == 'O' and button3['text'] == 'O'or
           button4['text'] == 'O' and button5['text'] == 'O' and button6['text'] == 'O'or
@@ -45,8 +47,10 @@ def ttt(buttons):
           button1['text'] == 'O' and button4['text'] == 'O' and button7['text'] == 'O'or
           button2['text'] == 'O' and button5['text'] == 'O' and button8['text'] == 'O'or
           button7['text'] == 'O' and button6['text'] == 'O' and button9['text'] == 'O'):
-          tkinter.messagebox.showinfo("Player O",'Winner is O !!!!')
-          restart_program()
+          if tkinter.messagebox.askyesno("Player X has won!",'Restart?') == True:
+               restart_program()
+          else:
+               sys.exit()
 
 
 buttons=StringVar()
