bb5c4d4685a657a1566f0945877c89815ca55aa6
Edward Wei <34778238+weied@users.noreply.github.com>
2019-01-26 18:47:03 -0800

    Update my tic tac 2.py

--- my tic tac 2.py
+++ my tic tac 2.py
@@ -25,7 +25,7 @@ def ttt(buttons):
           button1['text'] == 'X' and button4['text'] == 'X' and button7['text'] == 'X'or
           button2['text'] == 'X' and button5['text'] == 'X' and button8['text'] == 'X'or
           button7['text'] == 'X' and button6['text'] == 'X' and button9['text'] == 'X'):
-          tkinter.messagebox.showinfo("Player X",'Winner is X !!!!')
+          tkinter.messagebox.showinfo("Player O",'Winner is O !!!!')
           sys.exit()
           
      if(button1['text'] == 'O' and button2['text'] == 'O' and button3['text'] == 'O'or
@@ -42,6 +42,33 @@ def ttt(buttons):
 
 
 
-          
-     
-     
+buttons=StringVar()
+
+button1 = Button(tk,text=" ",font=('Times 20 bold'),bg='gray',fg='white',height=4,width=8,command=lambda:ttt(button1))
+button1.grid(row=1,column=0,sticky = S+N+E+W)
+
+button2 = Button(tk,text=' ',font=('Times 20 bold'),bg='gray',fg='white',height=4,width=8,command=lambda:ttt(button2))
+button2.grid(row=1,column=1,sticky = S+N+E+W)
+
+button3 = Button(tk,text=' ',font=('Times 20 bold'),bg='gray',fg='white',height=4,width=8,command=lambda:ttt(button3))
+button3.grid(row=1,column=2,sticky = S+N+E+W)
+
+button4 = Button(tk,text=' ',font=('Times 20 bold'),bg='gray',fg='white',height=4,width=8,command=lambda:ttt(button4))
+button4.grid(row=2,column=0,sticky = S+N+E+W)
+
+button5 = Button(tk,text=' ',font=('Times 20 bold'),bg='gray',fg='white',height=4,width=8,command=lambda:ttt(button5))
+button5.grid(row=2,column=1,sticky = S+N+E+W)
+
+button6 = Button(tk,text=' ',font=('Times 20 bold'),bg='gray',fg='white',height=4,width=8,command=lambda:ttt(button6))
+button6.grid(row=2,column=2,sticky = S+N+E+W)
+
+button7 = Button(tk,text=' ',font=('Times 20 bold'),bg='gray',fg='white',height=4,width=8,command=lambda:ttt(button7))
+button7.grid(row=3,column=0,sticky = S+N+E+W)
+
+button8 = Button(tk,text=' ',font=('Times 20 bold'),bg='gray',fg='white',height=4,width=8,command=lambda:ttt(button8))
+button8.grid(row=3,column=1,sticky = S+N+E+W)
+
+button9 = Button(tk,text=' ',font=('Times 20 bold'),bg='gray',fg='white',height=4,width=8,command=lambda:ttt(button9))
+button9.grid(row=3,column=2,sticky = S+N+E+W)
+
+tk.mainloop()
